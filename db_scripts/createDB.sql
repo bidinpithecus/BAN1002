82617760959954f058b9fc7e49bc356e45e370e6
bidinpithecus <vinibidin@gmail.com>
2023-05-30 18:44:56 -0300

    scripts not even close

---
+++ db_scripts/createDB.sql
@@ -0,0 +1,39 @@
+CREATE SCHEMA IF NOT EXISTS "main";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS "main"."transport_category" (
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "name" VARCHAR(50) UNIQUE NOT NULL,
+    "created_at" TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS "main"."passenger"(
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "name" VARCHAR(50) NOT NULL,
+    "phone" VARCHAR(25) UNIQUE NOT NULL,
+    "email" VARCHAR(128) NOT NULL,
+    "phone" VARCHAR(20) NOT NULL,
+    "created_at" TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS "main"."spent"(
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "name" VARCHAR(50) NOT NULL,
+    "date" TIMESTAMP DEFAULT current_timestamp,
+    "description" VARCHAR(100) NOT NULL,
+    "value" NUMERIC NOT NULL,
+    "category_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "created_at" TIMESTAMP DEFAULT current_timestamp,
+    FOREIGN KEY ("category_id") REFERENCES "main"."category",
+    FOREIGN KEY ("user_id") REFERENCES "main"."user"
+);
+
+INSERT INTO "main"."category" ("name") VALUES
+('Food'),
+('Fun'),
+('Education'),
+('Health'),
+('Transportation'),
+('Another');
---
+++ src/db.ts
@@ -0,0 +1,3 @@
+import postgres from "postgres";
+
+const sql = postgres('postgres://username:password@host:port/database');
